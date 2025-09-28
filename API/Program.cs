7377f45fbf5cece6a8491a5f6ccb40f1d2364349
manic2500 <manic2500@gmail.com>
2025-09-28 16:20:38 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,10 +11,14 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(opt => opt.WithOrigins("https://localhost:4200").AllowAnyHeader().AllowAnyMethod());
+
 app.MapControllers();
+
 
 app.Run();
 
